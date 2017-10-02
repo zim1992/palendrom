81e00e6b42d58f732860ae8262598dda7765f554
Corjan Zee <meset@live.nl>
2017-10-02 19:53:32 +0200

    Added Angular and SignalR

---
+++ PalendromeApplication/WebApplication3/Hubs/PalendromHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace WebApplication3.Hubs
+{
+    public class PalendromHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+    }
+}
---
+++ PalendromeApplication/WebApplication3/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebApplication3.Startup))]
+
+namespace WebApplication3
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
