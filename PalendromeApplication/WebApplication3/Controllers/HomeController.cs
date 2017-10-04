fc0f549d603b6afe30c822bf27a2249a3017edad
Corjan Zee <meset@live.nl>
2017-10-04 12:34:22 +0200

    worked on a method to generate words not palendroms

--- PalendromeApplication/WebApplication3/Controllers/HomeController.cs
+++ PalendromeApplication/WebApplication3/Controllers/HomeController.cs
@@ -5,14 +5,20 @@ using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.SignalR;
 using WebApplication3.Hubs;
+using System.Threading.Tasks;
 
 namespace WebApplication3.Controllers
 {
     public class HomeController : Controller
     {
+        IHubContext _hub = GlobalHost.ConnectionManager.GetHubContext<PalendromHub>();
+        private int maxValue = 13;
+        private int minValue = 4;
+        private int sizeOfWord = 0;
+        private string[] PalendromArray = new string[20];
+        private string[] letters = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "u", "t", "v", "w", "x", "y", "z" };
+        Random randNum = new Random();
 
-        
-        
         private static IList<string> _palendroms;
         
         
@@ -32,6 +38,28 @@ namespace WebApplication3.Controllers
             ViewBag.Message = "Your application description page.";
 
             return View();
+        }
+
+        private Task<string[]> CreatePalendroms()
+        {
+            return Task.Run(() =>
+            {
+                string[] PalendromWords = new string[20];
+                for (int i = 0; i < 20; i++)
+                {
+                    string tempWord = "";
+                    int sizeOfword = randNum.Next(minValue, maxValue);
+                    for (int j = 0; j < sizeOfword; j++)
+                        tempWord += letters[randNum.Next(letters.Length)];
+                    PalendromWords[i] = tempWord;
+                    
+
+                }
+                return PalendromWord;
+
+            });
+            
+            
         }
 
         public ActionResult Contact()
