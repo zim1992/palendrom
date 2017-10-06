96dc563d8304d4f8db62802e5b055fdd19de0ed9
Corjan Zee <meset@live.nl>
2017-10-06 12:10:05 +0200

    Added comments to the code

--- PalendromeApplication/WebApplication3/Hubs/PalendromHub.cs
+++ PalendromeApplication/WebApplication3/Hubs/PalendromHub.cs
@@ -33,9 +33,7 @@ namespace WebApplication3.Hubs
          **/ 
         public override Task OnConnected()
         {
-            int minValLoc = minValue;
-            int maxValLoc = maxValue;
-
+            Clients.All.Values(maxValue, minValue);
             if (_timer == null)
             {
                 _timer = new Timer(1000);
@@ -43,19 +41,25 @@ namespace WebApplication3.Hubs
 
                 _timer.Elapsed += (s, e) =>
                 {
+                    //clears the strings used for creating the palendroms
                     string tempWord = "";
-                    int sizeOfword = randNum.Next(minValLoc, maxValLoc);
+                    string wordHalf = "";
+                    //generates a size of word based on the max and min value
+                    int sizeOfword = randNum.Next(minValue, maxValue);
+                    //determines the halway point. 
                     double val = (sizeOfword / 2);
                     int halfway = Convert.ToInt32(Math.Floor(val));
+                    //sets the case back to one so it can create  an new palendrom
                     int caseVar = 1;
-                    string wordHalf="";
+
                     int i = 0;
                     for (int j = 0; j < sizeOfword; j++)
                     {
                         switch (caseVar)
                         {
-                            // creates the first half of the word and when it reaches half way it goes to case 2 
+                            // creates the first half of the word and when it reaches half way if Even drops to case 2 and odd it adds one more letter and drops to case 3   
                             case 1:
+                                //pulls a letter from the letter array. 
                                 tempWord += letters[randNum.Next(letters.Length)]; 
                                 if ((j+1) >= halfway) {                                                                        
                                     
@@ -67,19 +71,18 @@ namespace WebApplication3.Hubs
                                     else
                                     {
                                         tempWord += letters[randNum.Next(letters.Length)];
-                                        i = wordHalf.Length;
                                         caseVar = 3;
                                     }
                                     wordHalf = tempWord;
                                     i = wordHalf.Length;
                                 }                                    
                                 break;
-                            //Converts the word into a char array Determines if the word is an even or odd set of words if even it goes to case 3 and if odd then it goes to case 4
-                            
+                            //Case 2 generate words for even size words                                                  
                             case 2:
                                 tempWord += wordHalf.Substring((i-1),1);
                                 i--;
                                 break;
+                            //Case 3 generates words for odd sized words
                             case 3:
                                 if ((i-2) >= 0)
                                 {
@@ -89,11 +92,9 @@ namespace WebApplication3.Hubs
                                 break;
                         }
                     }
-
-                    Clients.All.palindromes(tempWord);                    
-                    Clients.All.Values(maxValue, minValue);
-                    minValLoc = minValue;
-                    maxValLoc = maxValue;
+                    //updates the client when a word is a
+                    Clients.All.palindromes(tempWord);
+                    
                 };
 
                 _timer.Start();
